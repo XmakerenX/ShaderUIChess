0d974a03b86ca7b142469cd549c4b932c1dd43c2
XmakerenX <XmakerenX@gmail.com>
2015-05-16 01:22:48 +0300

    added main menu. added pawns promotion menu. added main menu spin around board. fixed issue with UI not detecting correctly input after going fullscreen. *** still need to fix issue with going back from fullscreen.

---
+++ Debug/pawnsDef.h
@@ -0,0 +1,4 @@
+#define IDC_KNIGHT 201
+#define IDC_BISHOP 202
+#define IDC_ROOK 203
+#define IDC_QUEEN 204
---
+++ ShaderUIChess/MainMenuDef.h
@@ -0,0 +1,4 @@
+#define IDC_NEWGAME 201
+#define IDC_Continue 202
+#define IDC_OPTIONS 203
+#define IDC_EXIT 204
